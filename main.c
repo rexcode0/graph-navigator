1691827c80f190dd92e73efed4db8a4b6cd061c1
byte <pagal9759@gmail.com>
2024-11-14 23:25:28 +0530

    added navigation with wasd into the display function

--- main.c
+++ main.c
@@ -1,15 +1,60 @@
 // this is a code that will show an array displayed in a  square box and will have a highlighted
 // that can navigate through the complete array on the screen using w,s,d,a
 #include <stdio.h>
+#include <termios.h>
+#include <termios.h>
+
 void display(int cor_x, int cor_y, char display[20][21]) // code to display the display in the terminal
-{putchar(' ');
-    for (int i = 0; i < cor_y; i++) // loop to iteratete into y cordinate or the height of display
+{
+    char buff = 'o';
+    int x = 0, y = 0;
+    putchar(' ');
+    while (buff != 'q')
     {
-        for (int j = 0; j < cor_x; j++) // loop to iterate into x cordinate or width of the display
+        for (int i = 0; i < cor_y; i++) // loop to iteratete into y cordinate or the height of display
         {
-                putchar(display[i][j]);
-                putchar(' ');
+            for (int j = 0; j < cor_x; j++) // loop to iterate into x cordinate or width of the display
+            {
+                if (i == y && j == x)
+                {
+                    printf("\e[40m %c \e[0m",display[i][j]);
+                }
+                else
+                {
+                    putchar(display[i][j]);
+                    putchar(' ');
+                }
+            }
         }
+        buff =getchar();
+        switch (buff)
+        {
+        case 'w':
+            y--;
+            break;
+        case 's':
+            y++;
+            break;
+        case 'd':
+            x++;
+            break;
+        case 'a':
+            x--;
+            break;
+       
+        default:
+            if(y>=cor_y) 
+            {
+                x++;
+                y=0;
+            }
+            else if(x>=cor_x)
+            {
+                y++;
+                x=0;
+            }
+        }
+         printf("\033[H\033[J");
     }
 }
 
@@ -25,7 +70,7 @@ int main()
         {
             if (x == 20)
             {
-                page[y][x] = '\n';  
+                page[y][x] = '\n';
             }
             else
                 page[y][x] = '#';
